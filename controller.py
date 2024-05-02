7fbc8037278db4d6d0713187d6a0a179d02a8330
TeerapatTrepopsakulsin <teerapat.tre@ku.th>
2024-05-02 21:30:46 +0700

    Add controller module implementing MVC

--- controller.py
+++ controller.py
@@ -1,5 +1,32 @@
 """Controller module."""
+import tkinter as tk
+from tkinter import ttk
+from graph_generator import GraphGenerator, DefaultGraph
 
 
 class Controller:
-    pass
+    def __init__(self, graph_generator: GraphGenerator):
+        self.generator = graph_generator
+        self.display = None
+
+    def initialise(self, frame: ttk.Frame):
+        self.display = DefaultGraph(frame)
+
+        self.generator.graph = 'Histogram'
+        self.histgen = self.generator.get_generator(frame)
+
+        hist = self.histgen.generate()
+
+        graph1 = self.display.graph1()
+
+        des_stat = self.display.stat()
+
+        return hist, graph1, des_stat
+
+    def handle_select_year(self, frame: ttk.Frame, year):
+        self.generator.graph = 'Histogram'
+        self.generator.start_year = year
+        self.generator.end_year = year
+        self.histgen = self.generator.get_generator(frame)
+        hist = self.histgen.generate()
+        return hist
--- graph_ui.py
+++ graph_ui.py
@@ -3,12 +3,14 @@ from pages import *
 import tkinter as tk
 from tkinter import ttk, scrolledtext
 from winsound import MessageBeep, SND_NOWAIT
+from controller import *
 
 
 class GraphUI(tk.Tk):
-    def __init__(self):
+    def __init__(self, controller: Controller):
         super().__init__()
         self.title("Graph")
+        self.controller = controller
         #self.geometry('1280x720')
         self.init_components()
 
@@ -16,16 +18,14 @@ class GraphUI(tk.Tk):
         pass
 
     def init_components(self):
-        """Create components and layout the UI."""
         sticky = {'sticky': tk.NSEW}
         # notebook
         self.notebook = ttk.Notebook(self)
         self.notebook.grid(row=0, column=0, **sticky)
 
         ## create frames
-        frame1 = Storytelling(self.notebook, width=1280, height=720)
-        frame2 = DataExploration(self.notebook, width=1280, height=720)
-
+        frame1 = Storytelling(self.notebook, self.controller)
+        frame2 = DataExploration(self.notebook)
 
         frame1.grid(row=0, column=0, **sticky)
         frame2.grid(row=0, column=0, **sticky)
@@ -43,15 +43,11 @@ class GraphUI(tk.Tk):
         # filter_table
 
         # fill the window
-        for row in range(5):
+        for row in range(1):
             self.rowconfigure(row, weight=1)
-        for col in range(3):
+        for col in range(1):
             self.columnconfigure(col, weight=1)
 
     def run(self):
         """start the app, wait for events"""
         self.mainloop()
-
-
-if __name__ == '__main__':
-    import main
